a6b8f577a4c3b86318b69d14b4110ec9e3b944e1
karkrish2009 <karkrish2009@gmail.com>
2023-10-11 13:05:26 -0700

    Round4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 function App() {
+  // Click handler function defined here
+  function handleClick() {
+      alert('button clicked');
+  }
+
   return (
       <div className='App'>
-          {/* Define the click handler callback function anonymously */}
-          <button onClick={() => console.log('button clicked')}>
-              Click me!
-          </button>
+          {/* Pass the reference to the click handler to the onClick prop */}
+          <button onClick={handleClick}>Click me!</button>
       </div>
   );
 }
 
 export default App;
+
 
 
