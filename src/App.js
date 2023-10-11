98e1e885d5860b59588da3691782bdeef977c7c3
karkrish2009 <karkrish2009@gmail.com>
2023-10-11 11:44:59 -0700

    round 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let showModal = true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+          <div className={showModal === false ? 'hide' : 'show'}>
+              A Modal Element
+          </div>
+      </div>
   );
 }
 
 export default App;
+
