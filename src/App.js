8f9cbd4a28745361007c353d19595dd912be6fb2
karkrish2009 <karkrish2009@gmail.com>
2023-10-10 22:17:11 -0700

    need luck

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GameForm from './components/GameForm';
+import Landing from "./components/Landing";
+import GameList from './components/GameList';
+
 
 function App() {
+  const [gamesData, setGamesData] = useState([]);
+  const handleGamesData=(gamesData)=>{
+    setGamesData(gamesData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+      <Routes>
+        <Route path='/' element={<Landing />}></Route>
+        <Route path='/preferences' element={<GameForm />}></Route>
+        <Route path='/results' element={<GameList gamesData={gamesData} handleGamesData={handleGamesData}/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
