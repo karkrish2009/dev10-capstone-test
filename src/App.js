ed2c29e3e33b8d41f8168f0a60f0f204adaadbd6
karkrish2009 <karkrish2009@gmail.com>
2023-10-15 14:44:02 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import { Fragment } from 'react'; // Import Fragment out of the React package
+function HelloButton() {
+    return <button onClick={() => console.log('Hello!')}>Click</button>;
+}
 
 function App() {
-    const pet = {
-        name: 'Snoopy',
-        breed: 'Beagle',
-        age: 8,
-    };
-
     return (
-        <Fragment>
-            <h1>Name: {pet.name}</h1>
-            <p>Breed: {pet.breed}</p>
-            <p>Age: {pet.age} years</p>
-        </Fragment>
+        <div>
+            <h1>Hello World</h1>
+            <HelloButton />
+        </div>
     );
 }
 
