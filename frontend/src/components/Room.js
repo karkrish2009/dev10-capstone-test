8d9c39cb83b47c2c0d41fdfdc6907f009ce4cb95
karkrish2009 <karkrish2009@gmail.com>
2023-10-18 22:34:54 -0700

    JPA initial

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,11 +1,51 @@
 import RectangularDimensions from './RectangularDimensions';
 import TempSettings from './TempSettings';
 import { useParams, Link } from 'react-router-dom';
+import React from "react"
+import { useState, useEffect } from "react";
 
 
 export default function Room() {
     const { id } = useParams();
 
+    const [gameDetails, setGameDetails] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetchGameDetails(id)
+          .then(console.log(id))
+          .then((data) => {
+            setGameDetails(data);
+            console.log("gameDetails", gameDetails.roomName);
+          })
+          .catch((error) => {
+            console.error('Error fetching game details:', error);
+            setError(error);
+          });
+    }, [id]);
+
+
+      const fetchGameDetails = async(id) => {
+        try {
+          console.log("I am here ", id);
+          const response = await fetch(("http://localhost:8080/api/room/" + id),
+           {
+            method: "GET"
+            }
+          
+          );
+          
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+    
+          const data = await response.json();
+          console.log("data", data[0]);
+          return data[0];
+        } catch (error) {
+          throw new Error('Error fetching game details');
+        }
+      };
     const backgroundColorStyles = {
         backgroundColor: '#B5C689',
         padding: '20px',
@@ -21,8 +61,26 @@ export default function Room() {
     };
 
     return (
-        <div>
-            <p>I am here {id}</p>
+        <div style={backgroundColorStyles}>
+            <h1 style={{ textAlign: "center", fontFamily: "'Press Start 2P', sans-serif" }}>{gameDetails.roomName}</h1>
+            <p style={largerTextStyles}>Description: {gameDetails.roomDescription}</p>
         </div>
     );
 }
+
+/*
+return (
+    <div style={backgroundColorStyles}>
+     <h1 style={{ textAlign: "center", fontFamily: "'Press Start 2P', sans-serif" }}>{gameDetails.roomName}</h1> -->
+      <p style={largerTextStyles}>Release Date: {formattedReleaseDate}</p>
+      <p style={largerTextStyles}>Rating: {gameDetails.rating}/5</p>
+      <p style={largerTextStyles}>Metacritic Score: {gameDetails.metacritic}/100</p>
+      <p style={largerTextStyles}>Average Playtime: {gameDetails.playtime} hours</p>
+      <div>
+        <h2>Platforms</h2>
+        <ul>
+          {gameDetails.platforms.map((platform) => (
+            <li style={largerTextStyles} key={platform.platform.id}>{platform.platform.name}</li>
+          ))}
+        </ul>
+      </div> */
