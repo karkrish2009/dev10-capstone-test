e8e44490781cefeda5c8a7b10d3125c38813b2c1
karkrish2009 <karkrish2009@gmail.com>
2023-10-19 09:31:48 -0700

    initial end-to-end

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -5,7 +5,7 @@ import React from "react"
 import { useState, useEffect } from "react";
 
 
-export default function Room() {
+export default function  Room() {
     const { id } = useParams();
 
     const [gameDetails, setGameDetails] = useState(null);
